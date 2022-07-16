91bb54b905ebf79ae76b36e46cdbb856ad9e99d1
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-16 14:57:42 -0600

    two ways to declare a struct

--- structs/main.go
+++ structs/main.go
@@ -1,8 +1,14 @@
 package main
-type person struct {
 
+import "fmt"
+type person struct {
+	firstName string
+	lastName string
 }
 
 func main() {
-
+	// alex := person{"Alex", "anderson"}
+	alex := person{firstName: "Alex", lastName: "Anderson"}  // same as the above line but more dynamic
+	
+	fmt.Println(alex)
 }
