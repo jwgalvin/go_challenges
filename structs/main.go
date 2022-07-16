5b4131b778995287a40caad921f1a42b10804113
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-16 15:13:46 -0600

    re-added first version commented out

--- structs/main.go
+++ structs/main.go
@@ -13,6 +13,13 @@ type person struct {
 }
 
 func main() {
+	// alex := person{"Alex", "anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}  // same as the above line but more dynamic
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anders"
+	// fmt.Println(alex)
+	// fmt.Printf(" %+ v", alex)
 	jim := person{
 		firstName: "Jim",
 		lastName: "Gyms",
